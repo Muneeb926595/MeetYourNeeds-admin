4e7cca6c8b6f272ee63cf03becaf714a6bcf26cd
Muneeb Ur Rehman <muneebkhann0@gmail.com>
2021-01-30 11:03:02 +0500

    Admin Setup

---
+++ src/@modules/SidePannel/SidePannel.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "./Sidepannel.module.css";
+
+function SidePannel() {
+  return (
+    <div className={classes.sidePannelContainer}>
+      <div></div>
+    </div>
+  );
+}
+
+export default SidePannel;
